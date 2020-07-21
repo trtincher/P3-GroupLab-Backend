eb6bf3e3f9a8c8f3f7755277e0221091a736b430
jhall9871 <james@jameshallmusic.com>
2020-07-21 12:05:08 -0500

    get by email enabled

--- models/student.js
+++ models/student.js
@@ -1,4 +1,5 @@
 const mongoose = require("../db/connection");
+const Teacher = require("./teacher");
 
 const studentSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
@@ -17,6 +18,25 @@ const studentSchema = new mongoose.Schema({
   online: { type: Boolean, default: false },
   student: { type: Boolean, default: false },
   teacher: { type: Boolean, default: false },
+});
+
+// After the delete, cascade delete all references
+studentSchema.post("remove", (document) => {
+  res.send(document);
+  const studentId = document._id;
+  // console.log("studentId", studentId)
+  Teacher.find({ studentRoster: { $in: studentId } }).then((teachers) => {
+    // res.send(teachers);
+    Promise.all(
+      teachers.map((teacher) =>
+        Teacher.findOneAndUpdate(
+          teacher._id,
+          { $pull: { studentRoster: studentId } },
+          { new: true }
+        )
+      )
+    );
+  });
 });
 
 const Student = mongoose.model("Student", studentSchema);
